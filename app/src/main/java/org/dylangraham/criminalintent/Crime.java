13e68dc53873230f35fade764c96ba9cf94b30f2
Dylan Graham <dylan.graham@gmail.com>
2016-03-17 10:26:07 +1100

    Add date and solved properties

--- app/src/main/java/org/dylangraham/criminalintent/Crime.java
+++ app/src/main/java/org/dylangraham/criminalintent/Crime.java
@@ -1,13 +1,17 @@
 package org.dylangraham.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID id;
     private String title;
+    private Date date;
+    private boolean solved;
 
     public Crime() {
         this.id = UUID.randomUUID();
+        this.date = new Date();
     }
 
     public UUID getId() {
@@ -20,5 +24,21 @@ public class Crime {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isSolved() {
+        return solved;
+    }
+
+    public void setSolved(boolean solved) {
+        this.solved = solved;
     }
 }
