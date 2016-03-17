206a975b8a4481a905f7a562c2832bfce1803455
Dylan Graham <dylan.graham@gmail.com>
2016-03-17 23:07:14 +1100

    temp data for crimes

--- app/src/main/java/org/dylangraham/criminalintent/CrimeLab.java
+++ app/src/main/java/org/dylangraham/criminalintent/CrimeLab.java
@@ -12,6 +12,14 @@ public class CrimeLab {
 
     private CrimeLab(Context context) {
         crimes = new ArrayList<>();
+
+        // Temporary crime list
+        for (int i = 0; i < 100; ++i) {
+            Crime crime = new Crime();
+            crime.setTitle("Crime #" + i);
+            crime.setSolved(i % 3 == 0);
+            crimes.add(crime);
+        }
     }
 
     public static CrimeLab get(Context context) {
