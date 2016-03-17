58893d21ef438495fdb64b3fbb8ea2ba7b06169b
Dylan Graham <dylan.graham@gmail.com>
2016-03-17 17:54:49 +1100

    Initial commit

---
+++ app/src/main/java/org/dylangraham/criminalintent/CrimeLab.java
@@ -0,0 +1,36 @@
+package org.dylangraham.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class CrimeLab {
+    private static CrimeLab crimeLab;
+    private List<Crime> crimes;
+
+    private CrimeLab(Context context) {
+        crimes = new ArrayList<>();
+    }
+
+    public static CrimeLab get(Context context) {
+        if (crimeLab == null) {
+            crimeLab = new CrimeLab(context);
+        }
+        return crimeLab;
+    }
+
+    public List<Crime> getCrimes() {
+        return crimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : crimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
